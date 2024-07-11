dcba7d3478f60dc1bc05232638ec2f8bb60d71d9
Enzo Rossetto <enzo.rossetto@ifood.com.br>
2024-07-10 22:14:44 -0300

    Challenge 2
    Create a simple shopping list

--- Challenge 2 - 100 days of swift/ViewController.swift
+++ Challenge 2 - 100 days of swift/ViewController.swift
@@ -7,13 +7,58 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var shoppingList = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "Shopping list"
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addItem))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .trash, target: self, action: #selector(emptyList))
+    }
+    
+    @objc func addItem() {
+        let ac = UIAlertController(title: "Add item", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        let addItemAction = UIAlertAction(title: "Add", style: .default) { [weak self, weak ac] _ in
+            guard let item = ac?.textFields?.first?.text else { return }
+            self?.shoppingList.append(item)
+            
+            guard let itemsCount = self?.shoppingList.count else { return }
+            
+            let indexPath = IndexPath(row: itemsCount - 1, section: 0)
+            self?.tableView.insertRows(at: [indexPath], with: .automatic)
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        
+        ac.addAction(addItemAction)
+        ac.addAction(cancelAction)
+        present(ac, animated: true)
+    }
+    
+    @objc func emptyList() {
+        shoppingList.removeAll(keepingCapacity: true)
+        tableView.reloadData()
     }
 
-
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        shoppingList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Item", for: indexPath)
+        cell.textLabel?.text = shoppingList[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        shoppingList.remove(at: indexPath.row)
+        tableView.deleteRows(at: [indexPath], with: .automatic)
+    }
 }
 
